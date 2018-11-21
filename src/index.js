6d4ee8fcf7d1df2e819183a32e117740771f6cc5
Kanami <kanami.h217@gmail.com>
2018-11-21 20:51:25 +0900

    changed functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import _ from 'lodash';
-
+let numbers = []
+let sum = 0;
 function addRandomNum() {
-  const mainEl = document.getElementById('main');
-  let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
-  document.body.appendChild(p)
+      const mainEl = document.getElementById('main');
+      let p = document.createElement('p')
+      p.innerHTML = "Random Number: " + _.random(0, 10).toString();
+      document.body.appendChild(p)
+      if (numbers.length === 5) {
+        numbers.map(value => {
+          return value;
+        });
+      }
 }
+
 
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
+  const button2 = document.getElementById('button2')
+  button2.addEventListener("click", recordNumber);
   console.log("ready");
 }
+
+function recordNumber(){
+    const number = _.sum(addRandomNum);
+    const mainEl = document.getElementById('main');
+    let p = document.createElement('p')
+    p.innerHTML = "Sum of Numbers: " + number;
+    document.body.appendChild(p)
+};
